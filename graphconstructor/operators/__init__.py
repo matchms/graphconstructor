abeed4aba84f6ed240f9e9c2849b18a4e7255f65
Florian Huber <florian.huber@hs-duesseldorf.de>
2025-10-12 13:26:39 +0200

    add doubly stochastic algorithm

--- graphconstructor/operators/__init__.py
+++ graphconstructor/operators/__init__.py
@@ -1,10 +1,12 @@
 from .base import GraphOperator
+from .doubly_stochastic import DoublyStochastic
 from .knn_selector import KNNSelector
 from .marginal_likelihood import MarginalLikelihoodFilter
 from .weight_threshold import WeightThreshold
 
 
 __all__ = [
+    "DoublyStochastic",
     "GraphOperator",
     "KNNSelector",
     "MarginalLikelihoodFilter",
